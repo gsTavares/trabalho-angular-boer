9aec35cdf378fba22a44ae8d115b2f1168ab515d
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-03-09 18:37:33 -0300

    fix: path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/dist/trabalho-angular-boer/browser'));
