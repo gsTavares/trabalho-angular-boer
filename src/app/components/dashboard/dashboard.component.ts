dfdf9289a7bc5a18ad69d324a71b2ca37a5da53b
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-03-17 20:38:42 -0300

    feat: adding register to dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,24 +1,85 @@
-import { Component } from '@angular/core';
-import { DashboardService } from '../../services/dashboard.service';
+import { AfterViewChecked, AfterViewInit, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import ApexCharts from 'apexcharts';
+import { ChartComponent, NgApexchartsModule } from 'ng-apexcharts';
+import colors from 'tailwindcss/colors';
 import { ChartOptions } from '../../@types/chart-options';
-import { NgApexchartsModule } from 'ng-apexcharts';
+import { DashboardService } from '../../services/dashboard.service';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NgApexchartsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, NgApexchartsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
-  charts: ChartOptions[] = [];
+  @ViewChild('barChartElement') barChartElement?: ChartComponent
+
+  barChart: any;
+  barChartSeries: any
+
+  countries: string[] = [
+    'Brasil',
+    'China',
+    'Estados Unidos',
+    'Japão',
+    'Rússia',
+    'India',
+    'Canada'
+  ]
+
+  newRegisterForm?: FormGroup
+  formBuilder: FormBuilder = new FormBuilder();
+  submitted: boolean = false;
 
   constructor(private dashboardService: DashboardService) {
-    this.charts = [
-      this.dashboardService.lineChart,
-      this.dashboardService.areaChart
-    ];
+
+  }
+
+  ngOnInit(): void {
+    this.buildNewRegisterForm();
+    this.dashboardService.barChartOptions.subscribe({
+      next: (value) => {
+        this.barChart = value.chart;
+        this.barChartSeries = value.series
+        if (this.barChartElement) {
+          this.barChartElement.updateSeries(this.barChartSeries, true);
+        }
+      }
+    });
+  }
+
+  buildNewRegisterForm() {
+    this.newRegisterForm = this.formBuilder.group({
+      caseType: [null, Validators.required],
+      country: [null, Validators.required],
+      quantity: [null, [Validators.required, Validators.min(1)]]
+    });
+  }
+
+  updateSeries(form: FormGroupDirective) {
+    this.submitted = true;
+    if (this.newRegisterForm!.valid) {
+      const casosCadastrados = this.barChartSeries[0].data[0];
+      
+
+      casosCadastrados.y += this.newRegisterForm?.get('quantity')?.value;
+
+      if(this.newRegisterForm!.get('caseType')?.value === 'POSITIVO') {
+        const casosPositivos = this.barChartSeries[0].data[1];
+        casosPositivos.y += this.newRegisterForm?.get('quantity')?.value;
+      } else {
+        const casosNegativos = this.barChartSeries[0].data[2];
+        casosNegativos.y += this.newRegisterForm?.get('quantity')?.value;
+      }
+      this.dashboardService.barChartOptions.next({ ...this.dashboardService.barChartOptions.value, series: this.barChartSeries });
+      form.resetForm();
+      this.submitted = false;
+    }
+
   }
 
 }
--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,100 +1,40 @@
 import { Injectable } from '@angular/core';
 import { ChartOptions } from '../@types/chart-options';
 import colors from 'tailwindcss/colors'
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
-  lineChart: ChartOptions = {
+  barChartOptions: BehaviorSubject<ChartOptions> = new BehaviorSubject<ChartOptions>({
     chart: {
       height: 350,
-      type: "line",
-      stacked: false
-    },
-    dataLabels: {
-      enabled: false
-    },
-    colors: ["#FF1654", "#247BA0"],
-    series: [
-      {
-        name: "Positivos",
-        data: [1.4, 2, 2.5, 1.5, 2.5, 2.8, 3.8, 4.6],
-        color: colors.red[500]
-      },
-      {
-        name: "Negativos",
-        data: [20, 29, 37, 36, 44, 45, 50, 58],
-        color: colors.green[500]
-      }
-    ],
-    stroke: {
-      width: [4, 4]
+      type: 'bar'
     },
     plotOptions: {
       bar: {
-        columnWidth: "20%"
+        horizontal: true,
       }
     },
-    xaxis: {
-      categories: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
-    },
-    yaxis: [
-      {
-        axisTicks: {
-          show: true
-        },
-        axisBorder: {
-          show: true,
-          color: "#FF1654"
-        },
-        labels: {
-          style: {
-            colors: "#FF1654"
-          }
-        },
-        title: {
-          text: "Series A",
-          style: {
-            color: "#FF1654"
-          }
-        }
-      },
-      {
-        opposite: true,
-        axisTicks: {
-          show: true
-        },
-        axisBorder: {
-          show: true,
-          color: "#247BA0"
-        },
-        labels: {
-          style: {
-            colors: "#247BA0"
-          }
-        },
-        title: {
-          text: "Series B",
-          style: {
-            color: "#247BA0"
-          }
-        }
-      }
-    ],
-    tooltip: {
-      shared: false,
-      intersect: true,
-      x: {
-        show: false
-      }
-    },
-    legend: {
-      horizontalAlign: "left",
-      offsetX: 40
-    }
-  }
+    series: [{
+      name: 'Casos cadastrados',
+      data: [{
+        x: 'Número de casos cadastrados',
+        y: 95,
+        fillColor: colors.blue[500]
+      }, {
+        x: 'Positivos',
+        y: 36,
+        fillColor: colors.red[500],
+      }, {
+        x: 'Negativos',
+        y: 59,
+        fillColor: colors.green[500]
+      }]
+    }],
+  });
 
 
   areaChart: ChartOptions = {
@@ -117,7 +57,7 @@ export class DashboardService {
         shadeIntensity: 1,
         opacityFrom: 0.7,
         opacityTo: 0.9,
-        
+
       }
     },
     xaxis: {
